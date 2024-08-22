226e35718515b9c5a6c4f8234d2fe94c5510f46d
aathi-aathi <aathivdr2004@gmail.com>
2024-08-22 07:39:10 +0530

    notcompleted removed

--- express-server.js
+++ express-server.js
@@ -3,7 +3,6 @@ import connectToDb from './mongodb/mongodb-connect.js'
 import cors from 'cors'
 import getRouter from './challenge.js'
 import editRouter from './editRouter.js'
-import notCompletedRouter from './notCompleted.js'
 
 const app = express()
 app.use(express.json())
@@ -11,7 +10,7 @@ app.use(cors())
 await connectToDb()
 app.use('/challenge',getRouter)
 app.use('/edit',editRouter)
-app.use('/notCompleted',notCompletedRouter)
+
 const port = 7303
 app.listen(port,()=>{
     console.log('port',port ,'running...')
